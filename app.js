5cc1e41984946149b591d5d1faea9674d877d397
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-21 17:06:10 -0400

    add gallery js step 2 modal functionality

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ function Gallery(element) {
   this.closeBtn = getElement('.close-btn');
   this.prevBtn = getElement('.prev-btn');
   this.nextBtn = getElement('.next-btn');
+  //bind
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
   //event listener
   this.container.addEventListener(
     'click',
@@ -42,11 +46,31 @@ Gallery.prototype.openModal = function (selectedImage, list) {
     })
     .join('');
   this.modal.classList.add('open');
+  //add event listeners for modal
+  this.closeBtn.addEventListener('click', this.closeModal);
+  this.nextBtn.addEventListener('click', this.nextImage);
+  this.prevBtn.addEventListener('click', this.prevImage);
 };
 
 Gallery.prototype.setMainImage = function (selectedImage) {
   this.modalImg.src = selectedImage.src;
   this.imageName.textContent = selectedImage.title;
+};
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove('open');
+  //remove event listeners
+  this.closeBtn.removeEventListener('click', this.closeModal);
+  this.nextBtn.removeEventListener('click', this.nextImage);
+  this.prevBtn.removeEventListener('click', this.prevImage);
+};
+
+Gallery.prototype.nextImage = function () {
+  console.log('next');
+};
+
+Gallery.prototype.prevImage = function () {
+  console.log('prev');
 };
 
 //instances
