cbe3c341155450081f2404e8f7bec7d0c27970a6
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-21 16:31:06 -0400

    add gallery js step 2 open modal

--- app.js
+++ app.js
@@ -10,16 +10,44 @@ function getElement(selection) {
 
 //constructor
 function Gallery(element) {
+  this.container = element;
   this.list = [...element.querySelectorAll('.img')];
-  //target modal using document not elemet scope
+  //target modal using document not element scope
   this.modal = getElement('.modal');
-  this.modalImg = getElement('.modal-img');
+  this.modalImg = getElement('.main-img');
+  this.imageName = getElement('.image-name');
   this.modalImages = getElement('.modal-images');
   this.closeBtn = getElement('.close-btn');
   this.prevBtn = getElement('.prev-btn');
   this.nextBtn = getElement('.next-btn');
-  console.log(this);
+  //event listener
+  this.container.addEventListener(
+    'click',
+    function (e) {
+      if (e.target.classList.contains('img')) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
 }
+
+//proto
+Gallery.prototype.openModal = function (selectedImage, list) {
+  this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list
+    .map(function (image) {
+      return `<img src="${
+        image.src
+      }" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}"/>`;
+    })
+    .join('');
+  this.modal.classList.add('open');
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
+};
 
 //instances
 const nature = new Gallery(getElement('.nature'));
